ff91d8a8832cb800dcc5a697d5033e19a7910533
Pat Palmerston <patpalmerston@gmail.com>
2019-09-13 10:01:40 -0700

    get project by id working

--- projects/project-router.js
+++ projects/project-router.js
@@ -16,6 +16,20 @@ router.get('/', (req, res) => {
 		});
 });
 
+// find projects by id
+router.get('/:id', (req, res) => {
+	const { id } = req.params;
+
+	Projects.findById(id)
+		.then(project => {
+			res.status(200).json(project);
+		})
+		.catch(err => {
+			console.log(err);
+			res.status(500).json({ error: 'unable to find that project id' });
+		});
+});
+
 // add projects
 router.post('/', (req, res) => {
 	const projectData = req.body;
