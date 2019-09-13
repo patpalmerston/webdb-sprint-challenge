a134f67cc76578179d3e9302497dbd4b1d302f41
Pat Palmerston <patpalmerston@gmail.com>
2019-09-13 09:57:12 -0700

    adding projects works

--- projects/project-router.js
+++ projects/project-router.js
@@ -1,20 +1,33 @@
 const express = require('express');
 
-const Projects = require('./project-model')
+const Projects = require('./project-model');
 
-const router = express.Router()
+const router = express.Router();
 
+// find projects
 router.get('/', (req, res) => {
 	Projects.find()
-	.then(projects => {
-		res.status(200).json(projects)
-	})
-	.catch(err => {
-		console.log(err)
-		res.status(500).json({error: 'unable to find projects'})
-	})
-})
+		.then(projects => {
+			res.status(200).json(projects);
+		})
+		.catch(err => {
+			console.log(err);
+			res.status(500).json({ error: 'unable to find projects' });
+		});
+});
 
+// add projects
+router.post('/', (req, res) => {
+	const projectData = req.body;
 
+	Projects.add(projectData)
+		.then(project => {
+			res.status(200).json(project);
+		})
+		.catch(err => {
+			console.log(err);
+			res.status(500).json({ error: 'unable to create project' });
+		});
+});
 
 module.exports = router;
